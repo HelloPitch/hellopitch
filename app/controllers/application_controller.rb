949d4ab2b1ba9ec33b1d392353f28b13a60a1c97
Andrew Arrow <oneone+github@gmail.com>
2014-06-14 08:19:18 -0700

    check domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :check_domain
+
+  private
+
+  def check_domain
+    return if Rails.env != 'production'
+    domain = request.original_url.split('/')[2]
+    if domain != 'www.hellopitch.com'
+      redirect_to 'http://www.hellopitch.com/'
+    end
+  end
+
 end
