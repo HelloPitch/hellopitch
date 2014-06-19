2c5cf233ba7ceefb41924de9fefcc5c17dedc511
Andrew Arrow <oneone+github@gmail.com>
2014-06-19 06:12:32 -0700

    menu

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,8 +1,31 @@
 class SalesController < ApplicationController
 
   before_action :must_have_user
+  before_action :active_tab
 
   def index
+  end
+
+  def targets
+  end
+
+  def schedule
+  end
+
+  def profile
+  end
+
+  def payment
+  end
+
+  private
+
+  def active_tab
+    path = request.env['PATH_INFO']
+    @active = 'targets' if path.index('/targets')
+    @active = 'schedule' if path.index('/schedule')
+    @active = 'profile' if path.index('/profile')
+    @active = 'payment' if path.index('/payment')
   end
 
 end
